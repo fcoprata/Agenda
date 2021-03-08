346dc74bbefd43846ba8a717bbfb86013c8eaba2
fcoprata <fcoprata@alu.ufc.br>
2021-03-08 03:58:15 -0300

    Atualizações no Projeto Agenda

--- core/views.py
+++ core/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render,redirect
 from core.models import Evento
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # def index(request):
 #     return redirect('/agenda/')
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou Senha inválido")
+    return redirect('/')
+
+
+def login_user(request):
+    return render(request,'login.html')
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
